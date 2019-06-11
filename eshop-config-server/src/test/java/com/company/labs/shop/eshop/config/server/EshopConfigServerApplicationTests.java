2449fa0897ccdb44781d96010e56a3d74781ebc7
Nishant.Sonar <Nishant.Sonar@SYNPUNLTJ8QV1F2.synechron.com>
2019-06-11 17:56:23 +0530

    eshop application server configurations server.

---
+++ eshop-config-server/src/test/java/com/company/labs/shop/eshop/config/server/EshopConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.company.labs.shop.eshop.config.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EshopConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
