55415953b5c5ddb38e58de5255f4c3a32b7ba619
Nishant.Sonar <Nishant.Sonar@SYNPUNLTJ8QV1F2.synechron.com>
2019-06-11 15:22:38 +0530

    Added Swagger UI to Item, bootstrap for configurations

--- src/main/java/com/company/labs/shop/eshop/model/Item.java
+++ src/main/java/com/company/labs/shop/eshop/model/Item.java
@@ -9,6 +9,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -18,20 +20,24 @@ import lombok.RequiredArgsConstructor;
  * @author Nishant.Sonar
  *
  */
+
+@ApiModel(description = "All details about the Item. ")
 @Entity
 @Table(name = "ITEM")
 @Data
 @RequiredArgsConstructor
 @NoArgsConstructor
 public class Item {
-
+	@ApiModelProperty(notes = "The database generated employee ID")		
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
+	@ApiModelProperty(notes = "The item name")
 	@NonNull
 	private String name;
 
+	@ApiModelProperty(notes = "The item price")
 	@NonNull
 	private Long price;
 
--- src/main/java/com/company/labs/shop/eshop/model/LineItem.java
+++ src/main/java/com/company/labs/shop/eshop/model/LineItem.java
@@ -9,32 +9,35 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import io.micrometer.core.lang.NonNull;
+import io.swagger.annotations.ApiModel;
+import lombok.Builder.Default;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-
 @Entity
 @Table(name = "LINEITEM")
 @Data
-@RequiredArgsConstructor
 @NoArgsConstructor
-public class LineItem {
+public class LineItem implements Comparable<LineItem>{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
-	@NonNull
 	@OneToOne
 	private Item item;
 
-	@NonNull
 	private Long quantity;
 	
 	private Long discount;
 	
-	
 	//ref
 	@ManyToOne
 	private Order order;
+
+	@Override
+	public int compareTo(LineItem o) {
+		return o.getItem().getName().compareTo(this.getItem().getName());
+	}
+	
 }
